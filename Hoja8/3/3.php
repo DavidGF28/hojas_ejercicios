03d2aacf0597efa8cc26d4206a06c2b0d3e9ebac
DavidGF <DavidGuFu@gmail.com>
2019-12-17 10:56:57 +0100

    hoja8 ejer2 hecho iniciando ejer3

---
+++ Hoja8/3/3.php
@@ -0,0 +1,63 @@
+<?php
+
+class Usuario{
+    var $nombre="defecto";
+    private $edad;
+    protected $telefono;
+    private $apellidos="defecto";
+    private $nombreCompleto;
+    
+    public function getNombreCompleto (){
+        return $this->nombreCompleto;
+    }
+    
+    public function getApellidos(){
+        return $this->apellidos;
+    }
+    
+    public function setApellidos($apellidos){
+        $this->apellidos=$apellidos;
+        $this->concatenar();
+    }
+    
+    public function getNombre(){
+        return $this->nombre;
+    }
+    
+    public function getEdad (){
+        return $this->edad;
+    }
+    
+    public function getTelefono(){
+        return $this->telefono;
+    }
+    
+    public function setNombre($nombre){
+        $this->nombre=$nombre;
+        $this->concatenar();
+    }
+    
+    public function setEdad($edad){
+        $this->edad=$edad;
+        }
+       
+    public function setTelefono ($telefono){
+        $this->telefono=$telefono;
+    }
+    
+    private function concatenar(){
+        $this->nombreCompleto= $this->nombre . " " . $this->apellidos;
+    }
+    
+}
+
+$persona = new Usuario();
+echo $persona->nombre;
+$persona->setEdad(51);
+$persona->setTelefono("232323");
+$persona->setApellidos("vazquez rodrigez");
+var_dump($persona);
+$persona->nombre="Ramon";
+var_dump($persona);
+$persona->setNombre("Ramon");
+var_dump($persona);
